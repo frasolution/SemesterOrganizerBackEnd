ccb84836703182caa46a25c4de10bfe4cd75c83a
Yadullah Duman <yadullah.duman@maibornwolff.de>
2020-05-12 21:58:24 +0200

    Finalize configuration

--- semester-organizer-backend/src/main.ts
+++ semester-organizer-backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import * as config from 'config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const serverConfig: any = config.get('server');
+  const logger = new Logger('bootstrap');
+  const port = serverConfig.port;
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
   await app.listen(3000);
+  logger.log(`App is listening on port ${port}`);
 }
+
 bootstrap();
