24721774a1f9056686cb52a2e56674882900368d
Max M <meshstyles@users.noreply.github.com>
2020-06-20 16:25:40 +0200

    Added Courses delete, get, patch by id (tested)

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CoursesRepository } from './courses.repository';
+import { Course } from './courses.entity';
+import { EditCourseDto } from './dto/edit-course.dto';
 
 @Injectable()
 export class CoursesService {
@@ -8,4 +10,23 @@ export class CoursesService {
     @InjectRepository(CoursesRepository)
     private coursesRepository: CoursesRepository,
   ) {}
+
+  async findOne(courseId: number): Promise<Course> {
+    return await this.coursesRepository.findOne(courseId);
+  }
+
+  async removeOne(courseId: number): Promise<void> {
+    await this.coursesRepository.delete(courseId);
+  }
+
+  //TODO optimize
+  async updateOne(
+    editCourseDto: EditCourseDto,
+    courseId: number,
+  ): Promise<void> {
+    const course = await this.coursesRepository.findOne(courseId).catch();
+    course.courseName = editCourseDto.courseName;
+    await this.coursesRepository.update(courseId, course);
+    return;
+  }
 }
--- src/teams/teams.module.ts
+++ src/teams/teams.module.ts
@@ -6,12 +6,14 @@ import { TeamsService } from './teams.service';
 import { TeamsRepository } from './teams.repository';
 import { UsersModule } from '../users/users.module';
 import { AuthModule } from '../auth/auth.module';
+import { CoursesModule } from '../courses/courses.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([TeamsRepository]),
     UsersModule,
     AuthModule,
+    CoursesModule,
   ],
   controllers: [TeamsController],
   providers: [TeamsService],
